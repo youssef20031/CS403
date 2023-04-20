1493b6a81cad11ac6297e9950d6d7f57d5245d64
numberw8835 <119764846+numberw8835@users.noreply.github.com>
2023-04-20 21:50:21 +0200

    Update project.pl

--- project.pl
+++ project.pl
@@ -1,1 +1,11 @@
-
+% Predicate to update the teaching load of a TA
+ta_slot_assignment(TAs, RemTAs, Name) :-
+    % Find the TA with the given name
+    member(TA, TAs),
+    TA = ta(Name, Load),
+    % Decrement the teaching load of the TA by 1
+    NewLoad is Load - 1,
+    % Create a new TA structure with the updated teaching load
+    NewTA = ta(Name, NewLoad),
+    % Replace the old TA with the new TA in the list of TAs
+    select(TA, TAs, NewTA, RemTAs).
