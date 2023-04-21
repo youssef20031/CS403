9b534e8abccb2cc00940d19432f898c35e3f942b
Youssef Maged <43968405+youssef20031@users.noreply.github.com>
2023-04-21 13:05:31 +0200

    Update project.pl

--- project.pl
+++ project.pl
@@ -9,3 +9,23 @@ ta_slot_assignment(TAs, RemTAs, Name) :-
     NewTA = ta(Name, NewLoad),
     % Replace the old TA with the new TA in the list of TAs
     select(TA, TAs, NewTA, RemTAs).
+
+
+max_slots_per_day(DaySched, Max) :-
+    flatten(DaySched, [X|T]),
+    max_slots_per_day_helper([X|T], Max).
+
+
+max_slots_per_day_helper([],_).	
+max_slots_per_day_helper([X|T],Max):-
+    count_occurrences(X, [X|T], Count),
+    Count =< Max,
+    max_slots_per_day_helper(T,Max).
+
+count_occurrences(_, [], 0).
+count_occurrences(X, [X|T], N) :-
+    count_occurrences(X, T, N1),
+    N is N1 + 1.
+count_occurrences(X, [Y|T], N) :-
+    X \= Y,
+    count_occurrences(X, T, N).
